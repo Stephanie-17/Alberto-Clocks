6bbd8b1f94ea1e868f42e731c11b0adbd64253b3
Stephanie-17 <Stephanie>
2025-10-13 21:05:06 +0100

    created the Navbar

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import Navbar from './components/Navbar'
+
+function App() {
+  return (
+    <>
+    <Navbar />
+    {/* <img src="/hero.png" alt="" />
+     <h1>Alberto Clocks</h1>
+     <p>Luxury in  the making</p>
+     <button>Buy</button> */}
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+import "./Navbar.css";
+import { useState } from "react";
+const Navbar = () => {
+	const [menu, setMenu] = useState(false)
+	return (
+		<div className="nav-cont">
+			<img
+				className="logo"
+				src="/Logo.svg"
+				alt="Albert watch company clocks logo"
+			/>
+			<div    className="mobile-nav">
+				{!menu && (
+						<img className="menu-btn" onClick={()=>setMenu(true)} src="/menu-btn.svg" alt="mebu-btn" />
+				)}
+			
+				<div className={`mob-nav-items ${!menu && 'remove'}`}>
+					<img
+					onClick={()=> setMenu(false)}
+						src="/close-btn.svg"
+						className="close-btn"
+						alt="close-menu-btn"
+					/>
+
+					<a href="">Products</a>
+					<a href="">Technology</a>
+					<a href="">Store Locator</a>
+					<a href="">Support</a>
+					<a href="">About</a>
+					<a href="">Contact</a>
+					<a href="">Gallery</a>
+				</div>
+				<div className={`overlay ${!menu && 'remove-overlay'} `}></div>
+			</div>
+		</div>
+	);
+};
+
+export default Navbar;
